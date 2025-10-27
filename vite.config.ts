5a74f2d4593df1006fbce1e5ab8f9f2656dcdc05
haigiefarzal <haigiefarzaldev@gmail.com>
2025-10-27 08:41:14 +0700

    Update base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/whoareyoubyportofolio/', // ← ini penting, sama persis dengan nama repo kamu
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  base: '/maxwillbethewinner/', // 👈 penting untuk GitHub Pages
 });
